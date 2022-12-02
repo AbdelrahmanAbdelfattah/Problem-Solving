65d06324f2350221fabe4a9fcc59c38bef0b1026
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2022-12-02 04:55:07 +0200

    Time: 3 ms (84.01%), Space: 9.1 MB (17.72%) - LeetHub

---
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+  int maxDistance(vector<int>& colors) {
+        int n = colors.size(), i = 0, j = n - 1;
+        while (colors[0] == colors[j]) j--;
+        while (colors[n - 1] == colors[i]) i++;
+        return max(n - 1 - i, j);
+    }
+};
