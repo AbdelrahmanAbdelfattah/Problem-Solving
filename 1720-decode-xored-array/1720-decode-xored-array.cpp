26de1ecd9c9618cc95bc41036a26aa0c5a2234f9
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2022-12-02 04:26:53 +0200

    Time: 102 ms (5.60%), Space: 26.1 MB (34.96%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first)\
+    {
+        vector<int> v;
+        v.push_back(first);
+        for( int i =0 ;i< encoded.size() ;i++ )
+        {
+            v.push_back(first^encoded[i]);
+            first =first^encoded[i];
+        }
+        return v;
+        
+    }
+};
