d50ed5c77a519241f523df9a00ed65805f598e9f
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2022-12-26 22:56:00 +0200

    Time: 30 ms (83.13%), Space: 22.4 MB (13.30%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums)
+{
+    int sum =0 ;
+    for( int i=0 ;i< nums.size() ;i++ )
+    {
+        sum^=nums[i];
+    }
+    return sum;
+}
+};
