e8be7c53f6f047e36cfb73cc082a92bf067d8f3d
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2023-01-28 15:21:15 +0200

    Time: 10 ms (7.33%), Space: 8.3 MB (59.01%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
- int specialArray(vector<int>& nums)
+
+
+int specialArray(vector<int>& nums)
 {
-    for( int i =0 ;i<= nums.size() ;i++ )
+    int l =0 , r= nums.size();
+    while( l<=r)
     {
-        int n = 0 ;
-        for( int  j = 0 ;j < nums.size() ;j++ )
+        int mid = ( l+r)/2;
+        int n=0 ;
+        for( int i=0 ;i< nums.size() ;i++ )
         {
-            if( nums[j]>=i)
+            if(nums[i]>=mid)
                 n++;
         }
-        if( n == i)
-            return i;
+        if( n == mid )
+            return mid;
+        else if( n>mid )
+        {
+                        l= mid+1;
+
+        }
+        else
+        r= mid-1;
     }
     return -1;
 }
