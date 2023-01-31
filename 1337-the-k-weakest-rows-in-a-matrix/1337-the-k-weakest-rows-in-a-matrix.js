c71ee67834ee7c4643cbadfa72c944a895676d07
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2023-01-31 04:58:22 +0200

    Time: 61 ms (94.67%), Space: 43.9 MB (70.58%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -1,24 +1,21 @@
-class Solution {
-public:
-vector<int> kWeakestRows(vector<vector<int>>& mat, int k)
-{
-    vector<pair<int,int>> v;
-    for( int i=0 ;i< mat.size() ;i++ )
-    {
-        int n=0 ;
-        for( int j = 0 ;j< mat[i].size() ;j++ )
-        {
-            if( mat[i][j] )
-                n++;
-        }
-        v.push_back({n,i});
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */var kWeakestRows = function (mat, k) {
+  let ans = []
+  for (let i = 0; i < mat.length; i++) {
+    let n = 0;
+    for (let j = 0; j < mat[i].length; j++) {
+      if (mat[i][j]) n++;
     }
-    sort( v.begin() , v.end());
-    vector<int > ans ;
-    for( int i=0 ;i< k ; i ++ )
-    {
-        ans.push_back(v[i].second);
-    }
-    return ans;
-}
+    ans.push ( [n, i] );
+  }
+
+  ans.sort(function(a,b){return a[0]-b[0]});
+  let arr = [];
+  for (let i = 0; i < k; i++) {
+    arr[i] = ans[i][1];
+  }
+  return arr;
 };
