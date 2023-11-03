df7b437de17eff334631db8b434a70c0c846bfc6
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2023-11-04 00:53:10 +0200

    Time: 15 ms (96.92%), Space: 25.3 MB (76.53%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        
-    vector<int> v (nums.size());
-        
-    for (int i=0 ; i < nums.size() ;i++ )
-    {
-        int idx = (k%nums.size()+i)%nums.size();
-        v[idx] =nums[i];
-    }
-    
-        
-    for (int i=0 ; i < nums.size() ;i++ )
-    {
+   void rotate(vector<int>& nums, int k) {
 
-        nums[i] = v[i];
-    }
-        
-        
-        
-    }
+    // if the array is [{1,2,3},{4,5}] nad n is 2 we need to move last n element to the head of        the array
+    // to be [4,5,1,2,3] this will be done by reverse the hole array to be like this [5,4,3,2,1]        then
+    // reverse the first k elements then revers the rest of the array
+
+    reverse (nums.begin() ,nums.end() );
+
+    k %= nums.size();
+    reverse (nums.begin() ,nums.begin()+k );
+
+    reverse(nums.begin()+k,nums.end());
+
+
+}
 };
