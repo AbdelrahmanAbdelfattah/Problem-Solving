ed384f9f50cda39972ca830777961f9eb267aeb0
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2024-11-05 00:11:35 +0200

    Time: 18 ms (31.07%), Space: 55.5 MB (69.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -1,26 +1,29 @@
 var isCyclic = function (adj, vis, finish, node) {
   vis[node] = 1;
   let cyclic = 0;
-  for (let i = 0; i < adj[node].length; i++) {
-    let neighbor = adj[node][i];
-    if (vis[neighbor] === 0) {
+  for (let negh = 0; negh < adj[node].length; negh++) {
+    let neighbor = adj[node][negh];
+    if (vis[neighbor] === 0)
       cyclic = cyclic || isCyclic(adj, vis, finish, neighbor);
-    } else if (finish[neighbor] === 0) {
-      cyclic = 1;
-    }
+    else if (finish[neighbor] === 0) cyclic = 1;
   }
-  finish[node] = 1;  // This line should be outside of the loop
+  finish[node] = 1;
   return cyclic;
 };
 
 var canFinish = function (numCourses, prerequisites) {
-  let graph = new Array(numCourses).fill(0).map(() => []);
+  let graph = new Array(numCourses);
   let visit = new Array(numCourses).fill(0);
   let finish = new Array(numCourses).fill(0);
+  for (let i = 0; i < numCourses; i++) {
+    graph[i] = [];
+  }
 
   for (let i = 0; i < prerequisites.length; i++) {
     graph[prerequisites[i][1]].push(prerequisites[i][0]);
   }
+
+  console.log(graph);
 
   for (let course = 0; course < numCourses; course++) {
     if (visit[course] === 0) {
@@ -29,10 +32,3 @@ var canFinish = function (numCourses, prerequisites) {
   }
   return true;
 };
-
-// console.log(
-//   canFinish(2, [
-//     [1, 0],
-//     [0, 1],
-//   ])
-// );
