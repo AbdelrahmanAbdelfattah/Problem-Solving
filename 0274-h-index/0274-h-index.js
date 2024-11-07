cc48a2ca2721e9fc1c1e84aa156ef982da860d89
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2024-11-07 02:40:47 +0200

    Time: 2 ms (46.76%), Space: 49.3 MB (47.82%) - LeetHub

--- 0274-h-index/0274-h-index.js
+++ 0274-h-index/0274-h-index.js
@@ -1,14 +1,16 @@
 var hIndex = function (citations) {
-  citations.sort((a, b) => b - a);
+  // Sort in ascending order
+  citations.sort((a, b) => a - b);
   
   let length = citations.length;
   let h = 0; // h-index counter
   
+  // Iterate from the end of the sorted array
   for (let i = 0; i < length; i++) {
-    if (citations[i] >= i + 1) {
-      h = i + 1; 
-    } else {
-      break; 
+    // Calculate how many papers have at least `length - i` citations
+    if (citations[i] >= length - i) {
+      h = length - i;
+      break; // Once we find the h-index, we can stop
     }
   }
   
