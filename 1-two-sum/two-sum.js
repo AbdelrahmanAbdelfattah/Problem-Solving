c9c320632f3c4c2ea55087cc524c0f8703e9381d
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2026-04-08 09:46:35 +0200

    Time: 6 ms (42.65%) | Memory: 57.2 MB (8.46%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,6 +4,8 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // first solution with n2 time
+    /*
     for (let i =0 ; i< nums.length ; i++ )
     {
         let sum = nums[i ]; 
@@ -13,4 +15,24 @@ var twoSum = function(nums, target) {
                return [i , j ];
         }
     }
+    */
+
+    // second solution 
+    let mp = new Map (); 
+    for  (let i =0 ;i < nums.length ; i++ )
+    {
+        if (!mp.has(nums[i]))
+        {
+            mp.set(nums[i] , i);
+        }
+    }
+
+    for (let i = 0 ;i < nums.length ; i++)
+    {
+        if(mp.has(target - nums[i]) && i != mp.get(target-nums[i]) )
+        {
+            return [i , mp.get(target - nums[i])  ]  ;
+        }
+    }
+
 };
