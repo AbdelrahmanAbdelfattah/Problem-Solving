0be709224c8ab26da8325103acd489ef9bed50f6
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2026-04-09 18:38:03 +0200

    Time: 0 ms (100.00%) | Memory: 54.7 MB (32.47%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -1,7 +1,8 @@
 function rotate(matrix) {
     const n = matrix.length;
 
-
+// first we swap each element [i,j] -> [j,i]
+// swap each row 
     for (let i = 0; i < n; i++) {
         for (let j = i; j < n; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
