12458610e10dafd44a8686d2f459e28b310d1727
Abdelrahman Abdelfdattah <abdelrahmanmohamed702413@gmail.com>
2026-04-20 21:56:39 +0200

    Time: 41 ms (42.66%) | Memory: 80.8 MB (52.89%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,16 @@
+var longestConsecutive = function (nums) {
+  let numSet = new Set(nums);
+  let longestStreak = 0;
+  for (let num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (numSet.has(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  }
+  return longestStreak;
+};
